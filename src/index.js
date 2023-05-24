a6b90d70da5e52db180a0ef364d167585213d333
LongCao928 <17512055928@163.com>
2023-05-24 13:15:18 +0800

    reset

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './react-router-example/basic-data-router/App';
 // import App from './react-router-example/data-router/App';
-import App from './react-router-example/scroll-restoration/App';
-// import App from './App';
+// import App from './react-router-example/scroll-restoration/App';
+import App from './App';
 // import App from './react-router-example/auth/App';
 // import App from './react-router-example/custom-filter-link/App';
 // import App from './react-router-example/custom-link/App';
@@ -12,7 +12,7 @@ import App from './react-router-example/scroll-restoration/App';
 // import App from './react-router-example/modal/App'
 import reportWebVitals from './reportWebVitals';
 
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,12 +21,12 @@ root.render(
   // 与 Fragment 一样，StrictMode 不会渲染任何可见的 UI。它为其后代元素触发额外的检查和警告。
   <React.StrictMode>
     {/* Routes 创建的Route，使用 BrowserRouter */}
-    {/* <BrowserRouter>
+    <BrowserRouter>
       <App />
-    </BrowserRouter> */}
+    </BrowserRouter>
 
     {/* createBrowserRouter 创建Router，使用 RouterProvider */}
-    <App />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
