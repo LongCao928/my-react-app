8b8fa55576c2d1be0fea4db9140df8ea51661f27
LongCao928 <17512055928@163.com>
2023-05-28 20:41:36 +0800

    basic reduce overview

--- src/react-redux/store.js
+++ src/react-redux/store.js
@@ -23,3 +23,20 @@ export default configureStore({
 // Reduce核心思想：在应用程序中使用一个集中的位置来包含全局状态，并且在更新该状态时遵循特定的模式以使代码可预测。
 
 // stage management(状态管理)
+
+/** 术语
+ * Actions: 将操作视为描述应用程序中发生的事情的事件。
+ * state: 状态
+ * store: 存储当前 Reduce 应用程序的状态。
+ * getState: 获取当前状态的值。
+ * dispathch: 更新状态的唯一方法是调用 store.dispatch() 并传入一个 action 对象，store将运行它的 reducer 函数并将新的state值保存在里面。
+*/
+
+// Reduce 使用 "单向数据流" 应用程序结构。
+// state 描述了应用在某一时刻的状态， UI 会根据该状态进行渲染。
+// 当应用程序中发生了一些事情：UI 分发一个 action， store 运行 reducer，状态会根据发生的情况进行更新，
+// store 通知 UI 状态已经改变， UI 会根据新的状态重新渲染。
+
+
+
+
